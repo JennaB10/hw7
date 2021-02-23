c2e22afdf88cf00022a5b9ff15b46e084723abed
JennaB10 <jennb1046@gmail.com>
2021-02-22 20:25:16 -0600

    complete

--- movies.js
+++ movies.js
@@ -1,5 +1,5 @@
 firebase.auth().onAuthStateChanged(async function(user) {
-//window.addEventListener('DOMContentLoaded', async function(event) {
+
   if (user) { 
 
   let db = firebase.firestore()
@@ -65,32 +65,3 @@ ui.start('.sign-in-or-sign-out', authUIConfig) //placeholder for class we would 
 
   }
 })
-
-// Goal:   Refactor the movies application from last week, so that it supports
-//         user login and each user can have their own watchlist.
-
-// Start:  Your starting point is one possible solution for last week's homework.
-
-// Step 1: Add your Firebase configuration to movies.html, along with the
-//         (provided) script tags for all necessary Firebase services – i.e. Firebase
-//         Auth, Firebase Cloud Firestore, and Firebase UI for Auth; also
-//         add the CSS file for FirebaseUI for Auth.
-// Step 2: Change the main event listener from DOMContentLoaded to 
-//         firebase.auth().onAuthStateChanged and include conditional logic 
-//         shows a login UI when signed, and the list of movies when signed
-//         in. Use the provided .sign-in-or-sign-out element to show the
-//         login UI. If a user is signed-in, display a message like "Signed 
-//         in as <name>" along with a link to "Sign out". Ensure that a document
-//         is set in the "users" collection for each user that signs in to 
-//         your application.
-
-    //starts Firebase UI Aut
-
-
-// Step 3: Setting the TMDB movie ID as the document ID on your "watched" collection
-//         will no longer work. The document ID should now be a combination of the
-//         TMDB movie ID and the user ID indicating which user has watched. 
-//         This "composite" ID could simply be `${movieId}-${userId}`. This should 
-//         be set when the "I've watched" button on each movie is clicked. Likewise, 
-//         when the list of movies loads and is shown on the page, only the movies 
-//         watched by the currently logged-in user should be opaque.
